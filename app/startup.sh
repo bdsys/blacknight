cbfe7c4d61a566703799c2d88e15e46dafb4a95a
Andrew Lass <andrew.lass@sap.com>
2025-03-02 15:32:54 -0800

    scripts

--- app/startup.sh
+++ app/startup.sh
@@ -1,4 +1,7 @@
 echo "Starting up dev server..."
+echo "Pulling changes from git..."
+git checkout dev1
+git pull origin dev1
 echo "Starting API service in a detached container..."
 docker compose up --build -d
 echo "Done!"
